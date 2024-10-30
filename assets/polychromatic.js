8d737dc8660dc399ab895f6441009a3bea889235
Luke Horwell <code@horwell.me>
2024-10-30 19:01:23 +0000

    Tidy up JavaScript code
    - Use 'const' variables.
    - Use camel casing.
    - Remove unused "yes" check in table.

--- assets/polychromatic.js
+++ assets/polychromatic.js
@@ -1,11 +1,4 @@
-// Smooth loading between pages
-const swup = new Swup();
-swup.hooks.on("visit:start", pageExit);
-swup.hooks.on("content:replace", pageEnter);
-
-var sidebar_items = document.querySelectorAll(".sidebar-item");
-
-function smooth_scroll_to_top() {
+function scrollTop() {
     window.scroll({
         top: 0,
         left: 0,
@@ -13,37 +6,32 @@ function smooth_scroll_to_top() {
     });
 }
 
+// Smooth loading between pages
+const swup = new Swup();
+swup.hooks.on("visit:start", pageExit);
+swup.hooks.on("content:replace", pageEnter);
+
+const sidebarItems = document.querySelectorAll(".sidebar-item");
+
 function pageEnter() {
     // When navigation changes to another topic. New content replaced.
-    apply_markdown_tweaks();
-    sidebar_update_position();
-    setup_permalinks();
-    setup_toc();
+    sidebarUpdatePosition();
+    setupPermalinks();
+    tocSetup();
 }
 
 function pageExit() {
     // Upon leaving the current topic. Transition starts.
-    smooth_scroll_to_top();
+    scrollTop();
 }
 
-function apply_markdown_tweaks() {
-    // If page contains Yes/No in tables, add the fancy decorations.
-    var td = document.querySelectorAll("td");
-    if (td.length > 0) {
-        for (i = 0; i < td.length; i++) {
-            if (td[i].textContent.startsWith("Yes") === true)
-                td[i].classList.add("yes");
-        }
-    }
-}
-
-function sidebar_update_position() {
+function sidebarUpdatePosition() {
     // Make sure the correct item is selected
-    var active_sidebar_id = document.getElementById("current-page-id").value;
-    set_active_sidebar(document.getElementById(active_sidebar_id));
+    const activeID = document.getElementById("current-page-id").value;
+    setActiveSidebar(document.getElementById(activeID));
 
     // Scroll to selected item in sidebar
-    var selected = document.querySelector(".sidebar-item.active");
+    const selected = document.querySelector(".sidebar-item.active");
     if (selected) {
         document.querySelector(".sidebar").scroll({
             top: selected.offsetTop - 100,
@@ -53,29 +41,26 @@ function sidebar_update_position() {
     }
 }
 
-function set_active_sidebar(element) {
-    sidebar_items.forEach((item) => {
+function setActiveSidebar(element) {
+    sidebarItems.forEach((item) => {
         item.classList.remove("active");
     });
     if (element)
         element.classList.add("active");
 }
 
-function initial_page_load() {
-    apply_markdown_tweaks();
-    sidebar_update_position();
-    setup_permalinks();
-    setup_toc();
+function siteEnter() {
+    pageEnter();
 
     // Set up sidebar items to change active state
-    sidebar_items.forEach((item) => {
+    sidebarItems.forEach((item) => {
         item.onclick = function(e) {
-            set_active_sidebar(e.target);
+            setActiveSidebar(e.target);
         }
     });
 }
 
-function setup_permalinks() {
+function setupPermalinks() {
     const headings = document.querySelectorAll("h2, h3, h4");
     headings.forEach((header) => {
         header.innerHTML = `<a class="permalink" title="Permalink" href="#${header.id}"><img src="/assets/img/link.svg" alt="Link"/></a>` + header.innerHTML;
@@ -83,15 +68,14 @@ function setup_permalinks() {
 }
 
 // Table of contents - on right side
-var toc_scroll_timeout = null;
-
-function toc_on_scroll(event) {
+let tocScrollTimeout = null;
+function tocOnScroll(event) {
     // Timer based polling to prevent overloading
-    clearTimeout(toc_scroll_timeout);
-    toc_scroll_timeout = setTimeout(toc_update_pos, 50);
+    clearTimeout(tocScrollTimeout);
+    tocScrollTimeout = setTimeout(tocUpdatePosition, 50);
 }
 
-function setup_toc() {
+function tocSetup() {
     // Update the table of contents with page contents
     const toc = document.getElementById("toc");
     const headings = document.querySelectorAll(".topic h2, .topic h3, .topic h4");
@@ -109,34 +93,34 @@ function setup_toc() {
     document.getElementById("toc").firstChild.childNodes[0].classList.add("active");
 }
 
-function toc_update_pos() {
-    const current_pos = document.documentElement.scrollTop || document.body.scrollTop;
+function tocUpdatePosition() {
+    const currentPos = document.documentElement.scrollTop || document.body.scrollTop;
     const headings = document.querySelectorAll("h2, h3, h4");
     const items = document.querySelectorAll("#toc a");
-    var current_header = null;
+    let currentHeader = null;
 
     if (headings.length <= 1)
         return;
 
     headings.forEach((header) => {
-        if (header.offsetTop <= current_pos + 100) {
-            current_header = header;
+        if (header.offsetTop <= currentPos + 100) {
+            currentHeader = header;
         }
     });
 
-    if (!current_header)
-        current_header = headings[0]
+    if (!currentHeader)
+        currentHeader = headings[0]
 
     items.forEach((item) => {
         item.classList.remove("active");
-        if (item.attributes["data-target-id"].value == current_header.id) {
+        if (item.attributes["data-target-id"].value == currentHeader.id) {
             item.classList.add("active");
         }
     });
 }
 
 // On initial page load
-initial_page_load();
+siteEnter();
 
 // Track current position in table of contents
-window.onscroll = toc_on_scroll;
+window.onscroll = tocOnScroll;
